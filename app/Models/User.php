5c79ee7771d7263d26d20b2798a30796e1a21227
monika-simona <tancic.simona00@gmail.com>
2025-08-09 11:35:02 +0200

    modifikovanje User modela

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function podcasts(){
+        return $this->hasMany(Podcast::class);
+    }
 }
