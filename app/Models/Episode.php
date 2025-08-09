59065f32fc6df88ba55905c7c8b1fce65278a313
monika-simona <tancic.simona00@gmail.com>
2025-08-09 11:34:04 +0200

    kreiranje modela i migracija za Episode

---
+++ app/Models/Episode.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Episode extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['podcast_id','title','description', 'duration', 'release_date'];
+    public function podcast(){
+        return $this->belongsTo(Podcast::class);
+    }
+}
---
+++ database/migrations/2025_08_09_082727_create_episodes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('episodes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('podcast_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->integer('duration')->nullable(); // opis u minutima
+            $table->date('release_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('episodes');
+    }
+};
