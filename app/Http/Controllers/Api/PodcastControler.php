e00af5eeda7224dc0df9c3c8ee335de58c1fb5fb
monika-simona <tancic.simona00@gmail.com>
2025-08-09 17:39:22 +0200

    rutiranje Podcast

---
+++ app/Http/Controllers/Api/PodcastControler.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Podcast;
+use Illuminate\Http\Request;
+
+class PodcastControler extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $podcasts = Podcast::all();
+        return response()->json($podcasts);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'user_id' => 'required|exists:users,id',
+        ]);
+
+        $podcast = Podcaste::create($validated);
+
+        return response()->json($podcast,201);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Podcast $podcast)
+    {
+        return response()->json($podcast);
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Podcast $podcast)
+    {
+        $validated = $request->validate([
+            'title' => 'sometimes|required|string|max:255',
+            'description' => 'nullable|string',
+            'user_id' => 'sometimes|required|exists:users,id',
+        ]);
+
+        $podcast->update($validated);
+
+        return response()->json($podcast);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Podcast $podcast)
+    {
+        $podcast->delete();
+
+        return response()->json(null, 204);
+
+    }
+}
