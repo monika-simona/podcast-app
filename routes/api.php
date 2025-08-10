46a7e2348ea9371dcb55bf41c2b0c1f794b2410c
monika-simona <tancic.simona00@gmail.com>
2025-08-10 17:23:05 +0200

    Dodavanje middleware

--- routes/api.php
+++ routes/api.php
@@ -2,36 +2,36 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Controllers\Api\PodcastController;
 use Illuminate\Http\Controllers\Api\EpisodeController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-//ruta za prikaz svih podkastova
-Route::get('podcasts',[PodcastController::class, 'index']);
+//rute za korisnike
+Route::get('users', [UserController::class, 'index']);
+Route::post('users', [UserController::class, 'store']);
+Route::get('users/{id}', [UserController::class, 'show']);
+Route::put('users/{id}', [UserController::class, 'update']);
+Route::delete('users/{id}', [UserController::class, 'destroy']);
 
-//prikaz jednog podkasta preko id-ja
+
+// Rute dostupne svima
+Route::get('podcasts', [PodcastController::class, 'index']);
 Route::get('podcast/{id}', [PodcastController::class, 'show']);
-
-//kriranje novog podkasta
-Route::post('podcast', [PodcastController::class, 'store']);
-
-
-//izmena podkasta preko id-ja
-Route::put('podcast/{id}', [PodcastController::class, 'update']);
-
-//brisanje podkasta preko id-ja
-Route::delete('podcast/{id}', [PodcastController::class, 'destroy']);
-
-//za epizode
-
 Route::get('episodes', [EpisodeController::class, 'index']);
-
-Route::post('episodes', [EpisodeController::class, 'store']);
-
 Route::get('episodes/{id}', [EpisodeController::class, 'show']);
 
-Route::put('episodes/{id}', [EpisodeController::class, 'update']);
+// Rute koje zahtevaju autentifikaciju
+Route::middleware(['auth:sanctum', 'role:admin,author'])->group(function () {
+    // Podkast
+    Route::post('podcast', [PodcastController::class, 'store']);
+    Route::put('podcast/{id}', [PodcastController::class, 'update']);
+    Route::delete('podcast/{id}', [PodcastController::class, 'destroy']);
 
-Route::delete('episodes/{id}', [EpisodeController::class, 'destroy']);
+    // Epizoda
+    Route::post('episodes', [EpisodeController::class, 'store']);
+    Route::put('episodes/{id}', [EpisodeController::class, 'update']);
+    Route::delete('episodes/{id}', [EpisodeController::class, 'destroy']);
+});
